2ddbc6ba766221282e1351141d9c7d8ac9bd7db3
Laurence Yang <yufanyang@berkeley.edu>
2024-09-05 06:45:53 +0000

    Finished my first CS 162 assignment

--- hw-intro/words/main.c
+++ hw-intro/words/main.c
@@ -186,19 +186,23 @@ int main (int argc, char *argv[]) {
   if ((argc - optind) < 1) {
     // No input file specified, instead, read from STDIN instead.
     infile = stdin;
-    total_words += num_words(infile);
-    count_words(&word_counts, infile);
+    if (count_mode) {
+      total_words += num_words(infile);
+    } else {
+      count_words(&word_counts, infile);
+    }
   } else {
     // At least one file specified. Useful functions: fopen(), fclose().
     // The first file can be found at argv[optind]. The last file can be
     // found at argv[argc-1].
     for (int i = optind; i < argc; i++) {
       infile = fopen(argv[i], "r");
-      total_words += num_words(infile);
       fclose(infile);
-      infile = fopen(argv[i], "r");
-      count_words(&word_counts, infile);
-      fclose(infile);
+      if (count_mode) {
+        total_words += num_words(infile);
+      } else {
+        count_words(&word_counts, infile);
+      }
     }
   }
 
