8d40ac318358da022c816dc1dc7fc83aa60da960
Laurence Yang <yufanyang@berkeley.edu>
2024-11-08 06:19:19 +0000

    Finished mm_malloc

--- hw-memory/mm_alloc/mm_alloc.c
+++ hw-memory/mm_alloc/mm_alloc.c
@@ -9,18 +9,74 @@
 #include <stdio.h>
 #include <string.h>
 
-void* mm_malloc(size_t size) {
-  //TODO: Implement malloc
+struct mm_meta* head = NULL;
+struct mm_meta* tail = NULL;
 
+void* mm_malloc(size_t size) {
+  if (size == 0) {
+    return NULL;
+  }
+
+  if (head == NULL) {
+    void* vaddr = sbrk(size + sizeof(struct mm_meta));
+    if (vaddr == (void*)-1) {
+      return NULL;
+    }
+    struct mm_meta* new_meta = (struct mm_meta*)vaddr;
+    head = new_meta;
+    tail = new_meta;
+    new_meta->size = size;
+    new_meta->free = 0;
+    new_meta->next = NULL;
+    new_meta->prev = NULL;
+  } else {
+    struct mm_meta* cur = head;
+    while (cur != NULL) {
+      if (cur->size >= size + sizeof(struct mm_meta) && cur->free == 1) {
+        size_t unused = cur->size - size - sizeof(struct mm_meta);
+        if (unused <= sizeof(struct mm_meta)) {
+          cur->size = size;
+          cur->unused = unused;
+          cur->free = 0;
+          memset(cur + sizeof(struct mm_meta), 0, size + unused);
+        } else {
+          struct mm_meta* new_meta = (struct mm_meta*)(cur + cur->size + sizeof(struct mm_meta));
+          new_meta->size = cur->size - sizeof(struct mm_meta);
+          new_meta->unused = 0;
+          new_meta->free = 1;
+          new_meta->prev = cur;
+          new_meta->next = cur->next;
+          memset(new_meta + sizeof(struct mm_meta), 0, size);
+
+          cur->size = size;
+          cur->unused = 0;
+          cur->free = 0;
+          if (cur != tail) {
+            cur->next->next->prev = new_meta;
+          } else {
+            tail = new_meta;
+          }
+          cur->next = new_meta;
+          memset(cur + sizeof(struct mm_meta), 0, size);
+        }
+        return (void*)(cur + sizeof(struct mm_meta));
+      } else {
+        cur = cur->next;
+      }
+    }
+  }
   return NULL;
 }
 
 void* mm_realloc(void* ptr, size_t size) {
   //TODO: Implement realloc
-
-  return NULL;
+  ptr = NULL;
+  size = 0;
+  return (void*)(ptr+size);
 }
 
 void mm_free(void* ptr) {
   //TODO: Implement free
+  ptr = NULL;
+  if (ptr == NULL) return;
 }
--- hw-memory/mm_alloc/mm_alloc.h
+++ hw-memory/mm_alloc/mm_alloc.h
@@ -11,6 +11,14 @@
 
 #include <stdlib.h>
 
+struct mm_meta {
+    size_t size;
+    size_t unused;
+    int free;
+    struct mm_meta* next;
+    struct mm_meta* prev;
+};
+
 void* mm_malloc(size_t size);
 void* mm_realloc(void* ptr, size_t size);
 void mm_free(void* ptr);
